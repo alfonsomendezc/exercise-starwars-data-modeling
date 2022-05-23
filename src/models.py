26bdcdae3819c68f6820dc7050f7489cdff11176
alfonsomendezc <alfonsomendez2209@gmail.com>
2022-05-23 19:10:49 +0000

    v1 instagram

--- src/models.py
+++ src/models.py
@@ -8,49 +8,62 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(60), nullable=False)
-    last_name = Column(String(60), nullable=False)
-    email_address = Column(String(60), nullable=False)
-    address = Column(String(250))
-    phone_number = Column(String(30), nullable=False)
-    creation_date = Column(String(65), nullable=False)
+    username = Column(String(250)) 
+    email = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
 
-class Planet(Base):
-    __tablename__ = 'planet'
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    name = Column(String(60), nullable=False)
-    location = Column(String(250), nullable=False)
-    galaxy = Column(String(250), nullable=False)
-    weight = Column(Integer, nullable=False)
-    fauna = Column(String(250), nullable=False)
-    race = Column(String(250), nullable=False)
-
-class Character(Base):
-    __tablename__ = 'character'
+    user_id = Column(Integer, ForeignKey('user.id'))
+   
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    name = Column(String(60), nullable=False)
-    last_name = Column(String(60), nullable=False)
-    group = Column(String(30), nullable=False)
-    race = Column(String(60), nullable=False)
-    job = Column(String(60), nullable=False)
+    url = Column(String(250))
+    type = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post')
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    planet_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
-    character_id = Column(Integer, ForeignKey("character.id"), nullable=False)
-    user = relationship(User)
-    planet = relationship(Planet)
-    character = relationship(Character)
+    author_id = Column(String, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_content = Column(String(50))
+    post = relationship('Post')
+    user = relationship('User')
 
-def to_dict(self):
-    return {}
+class Like(Base):
+    __tablename__ = "like"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id")) 
+    post = relationship("post")
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
+class Following(Base):
+    __tablename__ = 'following'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
